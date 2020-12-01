c5141ca5d8b4cf84717a88ad5ff94e65bb467fca
stward1 <60272244+stward1@users.noreply.github.com>
2020-12-01 00:37:28 -0500

    updated customer insert

--- Models/InsertCustomerData.cs
+++ Models/InsertCustomerData.cs
@@ -1,6 +1,6 @@
 using API.Models.Interfaces;
 using System.Collections.Generic;
-using System.Data.SQLite;
+using MySql.Data.MySqlClient;
 using System;
 using System.IO;
 
@@ -10,38 +10,57 @@ namespace API.Models
 	{
         public void SaveCustomer(string FirstName, string LastName, string Email, string Password, string PhoneNumber, int RewardsPoints)
         {
-            string cs = $"URI=file:{Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "Database\\TitleTownCardsDatabase.db")}";
-			using var con = new SQLiteConnection(cs);
-			con.Open();
+            //connecting to and opening the database
+            DBConnect db = new DBConnect();
+            bool isOpen = db.OpenConnection();
 
-			using var cmd = new SQLiteCommand(con);
+            if (isOpen)
+            {
+                //if the open succeeded, we proceed with the sql commands
+                MySqlConnection con = db.GetCon();
 
-			cmd.CommandText = @"INSERT INTO Customer(`Customer Email`, `Customer First Name`, `Customer Last Name`, `Customer Phone Number`, `Rewards Points`, `Password`) VALUES(@CustomerEmail, @CustomerFirstName, @CustomerLastName, @CustomerPhoneNumber, @RewardsPoints, @Password)";
-			cmd.Parameters.AddWithValue("@CustomerEmail", Email);
-			cmd.Parameters.AddWithValue("@CustomerFirstName", FirstName);
-			cmd.Parameters.AddWithValue("@CustomerLastName", LastName);
-			cmd.Parameters.AddWithValue("@CustomerPhoneNumber", PhoneNumber);
-			cmd.Parameters.AddWithValue("@RewardsPoints", RewardsPoints);
-			cmd.Parameters.AddWithValue("@Password", Password);
-			cmd.Prepare();
-			cmd.ExecuteNonQuery();
+				string stm = @"INSERT INTO Customer(`Customer Email`, `Customer First Name`, `Customer Last Name`, `Customer Phone Number`, `Rewards Points`, `Password`) VALUES(@CustomerEmail, @CustomerFirstName, @CustomerLastName, @CustomerPhoneNumber, @RewardsPoints, @Password)";
+				MySqlCommand cmd = new MySqlCommand(stm, con);
+
+				cmd.Parameters.AddWithValue("@CustomerEmail", Email);
+				cmd.Parameters.AddWithValue("@CustomerFirstName", FirstName);
+				cmd.Parameters.AddWithValue("@CustomerLastName", LastName);
+				cmd.Parameters.AddWithValue("@CustomerPhoneNumber", PhoneNumber);
+				cmd.Parameters.AddWithValue("@RewardsPoints", RewardsPoints);
+				cmd.Parameters.AddWithValue("@Password", Password);
+				cmd.Prepare();
+				cmd.ExecuteNonQuery();
+
+				db.CloseConnection();
+			}
         }
 
 		public int UpdateRewardsPoints(Customer value)
 		{
-            string cs = $"URI=file:{Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "Database\\TitleTownCardsDatabase.db")}";
-			using var con = new SQLiteConnection(cs);
-			con.Open();
+            //connecting to and opening the database
+            DBConnect db = new DBConnect();
+            bool isOpen = db.OpenConnection();
 
-			using var cmd = new SQLiteCommand(con);
+            if (isOpen)
+            {
+                //if the open succeeded, we proceed with the sql commands
+                MySqlConnection con = db.GetCon();
 
-			cmd.CommandText = @"UPDATE Customer SET `Rewards Points` = @RewardsPoints WHERE `Customer Email` = @CustomerEmail";
-			cmd.Parameters.AddWithValue("@CustomerEmail", value.Email);
-			cmd.Parameters.AddWithValue("@RewardsPoints", value.RewardsPoints);
-			cmd.Prepare();
-			cmd.ExecuteNonQuery();			
+				string stm = @"UPDATE Customer SET `Rewards Points` = @RewardsPoints WHERE `Customer Email` = @CustomerEmail";
+				MySqlCommand cmd = new MySqlCommand(stm, con);
 
-			return value.RewardsPoints;
+				cmd.Parameters.AddWithValue("@CustomerEmail", value.Email);
+				cmd.Parameters.AddWithValue("@RewardsPoints", value.RewardsPoints);
+				cmd.Prepare();
+				cmd.ExecuteNonQuery();			
+
+				db.CloseConnection();
+
+				return value.RewardsPoints;
+			}
+
+			//-1 is returned if something goes wrong; -1 is a recognizable sentinel value across the system
+			return -1;
 		}
 	}
 }
